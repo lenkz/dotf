a3ed7bb7dc7412b8cb84307597e17520501e0107
lenkz <lenkza@gmail.com>
2013-11-26 20:01:23 +0700

    add pwd

--- ex.sh
+++ ex.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mv ~/.vim ~/.vim.bak
-mv ~/.vimrc ~/.vimrc.bak
-mv ~/.tmux.conf ~/.tmux.conf.bak
+#mv ~/.vim ~/.vim.bak
+#mv ~/.vimrc ~/.vimrc.bak
+#mv ~/.tmux.conf ~/.tmux.conf.bak
 
-ln -s .vim ~/.vim
-ln -s .vimrc ~/.vimrc
-ln -s .tmux.conf ~/.tmux.conf
+ln -s $PWD/.vim ~/.vim
+ln -s $PWD/.vimrc ~/.vimrc
+ln -s $PWD/.tmux.conf ~/.tmux.conf
