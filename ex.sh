4e956de0800e5be0d08403da29f6af96afb34b31
ratchakrit <ratchakrit@thevcgroup.com>
2013-11-27 14:08:53 +0700

    edit ex.sh for recommendation user

--- ex.sh
+++ ex.sh
@@ -11,7 +11,10 @@ ln -s $PWD/.tmux.conf ~/.tmux.conf
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 echo "install configured bundles:"
 echo "Launch vim, run :BundleInstall"
-cd ~/.vim/bundle/YouCompleteMe
-./install.sh --clang-completer
-cd -
-cp .ycm_extra_conf.py ~/.vim/bundle/YouCompleteMe/cpp/ycm/.ycm_extra_conf.py 
+echo "After Bundle install use these commands"
+
+echo "cd ~/.vim/bundle/YouCompleteMe"
+echo "./install.sh --clang-completer"
+echo "cd -"
+echo "cp .ycm_extra_conf.py ~/.vim/bundle/YouCompleteMe/cpp/ycm/.ycm_extra_conf.py" 
+
